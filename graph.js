b4908cb96482fb631031c4e9a4d55b14c1c55397
Bob German <bgerman1@hotmail.com>
2021-03-02 12:18:22 -0500

    Refactor for easier adding of learn modules

--- graph.js
+++ graph.js
@@ -1,17 +1,17 @@
-
-    // Create an authentication provider
-    const authProvider = {
-        getAccessToken: async () => {
-            // Call getToken in auth.js
-            return await getToken();
-        }
-    };
-    // Initialize the Graph client
-    const graphClient = MicrosoftGraph.Client.initWithMiddleware({ authProvider });
-    //Get user info from Graph
-    async function getUser() {
-        return await graphClient
-            .api('/me')
-            .select('id,displayName')
-            .get();
+// Create an authentication provider
+const authProvider = {
+    getAccessToken: async () => {
+        // Call getToken in auth.js
+        return await getToken();
     }
+};
+// Initialize the Graph client
+const graphClient = MicrosoftGraph.Client.initWithMiddleware({ authProvider });
+//Get user info from Graph
+async function getUser() {
+    ensureScope('user.read');
+    return await graphClient
+        .api('/me')
+        .select('id,displayName')
+        .get();
+}
